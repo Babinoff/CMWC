5a29cbbc4ccd91d905abb422a46ee05a83b39769
ababinov <a.n.babinov@we-on.com>
2025-12-10 16:13:14 +0300

    Fix CI: Enable package-lock.json and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: process.env.GITHUB_PAGES ? '/CollisionMatrixWithCostV3/' : '/',
+      base: process.env.GITHUB_PAGES ? '/CMWC/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
